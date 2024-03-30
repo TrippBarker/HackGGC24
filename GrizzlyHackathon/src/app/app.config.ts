a6533c6d7bd40ae95ff2db683fae64ea9fc3267c
Tripp Barker <colemanbarker@gmail.com>
2024-03-30 11:45:09 -0400

    init firebase

--- GrizzlyHackathon/src/app/app.config.ts
+++ GrizzlyHackathon/src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"grizzlyhackathon","appId":"1:251106817884:web:12573b2b36ceb0a2a57399","storageBucket":"grizzlyhackathon.appspot.com","apiKey":"AIzaSyCgnBVuBJb3P9uF0KFTt0e1CaV8031uDeQ","authDomain":"grizzlyhackathon.firebaseapp.com","messagingSenderId":"251106817884","measurementId":"G-146EDTRMCV"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
